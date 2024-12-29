3dfbe538969491b747e8399af03d9c41eeb53f4d
Cheps <cheps55@users.noreply.github.com>
2024-12-29 15:37:43 +0800

    Show sum at calendar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { Dropdown } from 'react-native-element-dropdown';
 import useAuth from './hook/useAuth';
@@ -15,7 +15,7 @@ function App() {
 	const cloudStorage = useFirebase();
 	const language = useLanguage();
 
-	const [sum, setSum] = useState(0);
+	const [sum, setSum] = useState({});
 	const [record, setRecord] = useState({});
 	const [inputData, setInputData] = useState({
 		name: '',
@@ -31,6 +31,7 @@ function App() {
 		timestamp: 0,
 	});
 	const { dateString, year, month, day, timestamp } = date;
+	const key = `${year}-${month}-${day}`;
 	const [dropdownItem, setDropdownItem] = useState([
 		{label: language.get('dropdown.food'), value: 'food'},
 		{label: language.get('dropdown.entertainment'), value: 'entertainment'},
@@ -38,43 +39,43 @@ function App() {
 	const [group, setGroup] = useState(dropdownItem[0]);
 
 	useEffect(() => {
-		const initData = async () => {
-			const current = new Date();
-			const _year = current.getFullYear();
-			const _month = String(current.getMonth() + 1).padStart(2, '0');
-			const _day = String(new Date().getDate()).padStart(2, '0');
-			for (const d of getDayInMonth(_year, month)) {
-				const history = await localStorage.get(`${_year}-${_month}-${d}`);
-				setRecord(prev => {
-					prev[d] = Array.isArray(history) ? history : [];
-					return prev;
-				});
-			}
-			setDate({dateString: `${_year}-${_month}-${_day}`, timestamp: new Date(`${_year}-${_month}-${_day}`), year: _year, month: _month, day: _day});
-        };
-        initData();
+		const current = new Date();
+		const _year = current.getFullYear();
+		const _month = String(current.getMonth() + 1).padStart(2, '0');
+		const _day = String(new Date().getDate()).padStart(2, '0');
+        initRecordList(_year, _month, _day);
 	}, []);
 
-	useEffect(() => {
-		record?.[day]?.forEach(item => {
-			setSum((prev) =>{
-				prev += Number(item.value);
-				return prev;
+	const initRecordList = async (_year, _month, _day) => {
+		for (const d of getDayInMonth(_year, month)) {
+			const _key = `${_year}-${_month}-${d}`;
+			const history = await localStorage.get(_key);
+			setRecord(prev => {
+				prev[_key] = Array.isArray(history) ? history : [];
+				return {...prev};
 			});
-		})
-	}, [dateString]);
+			setSum(prev => {
+				prev[_key] = 0;
+				(Array.isArray(history) ? history : []).forEach(item => {
+					prev[_key] += item.value;
+				});
+				return {...prev};
+			});
+		}
+		setDate({dateString: `${_year}-${_month}-${_day}`, timestamp: new Date(`${_year}-${_month}-${_day}`), year: _year, month: _month, day: _day});
+	};
 
 	const onSync = async () => {
 		const result = await localStorage.getAll();
-		for (const key of Object.keys(result)) {
-			await cloudStorage.set(key, result[key]);
+		for (const _key of Object.keys(result)) {
+			await cloudStorage.set(_key, result[_key]);
 		}
 	};
 
 	const onConfirm = async () => {
 		if (value.length === 0 || !date) { return; }
 
-		const _history = record?.[day] ?? [];
+		const _history = record?.[key] ?? [];
 		const data = {
 			id: _history.length > 0 ? _history[_history.length - 1].id + 1 : 1,
 			timestamp: timestamp,
@@ -85,46 +86,28 @@ function App() {
 		};
 		const list = [..._history, data];
 		await localStorage.set(dateString, list);
-		setSum((prev) =>{
-			prev += Number(value);
-			return prev;
-		});
 		setRecord(prev => {
-			prev[day] = list;
-			return prev;
+			prev[dateString] = list;
+			return {...prev};
 		});
-		setInputData({
-			name: '',
-			value: '',
-			tag: [],
+		setSum((prev) =>{
+			prev[dateString] += Number(value);
+			return {...prev};
 		});
+		setInputData({ name: '', value: '', tag: [] });
 	};
 
 	const onClear = async (item) => {
-		const newRecord = record?.[day]?.filter(x => x.id !== item.id);
+		const newRecord = record?.[key]?.filter(x => x.id !== item.id);
 		await localStorage.set(dateString, newRecord);
-		setSum((prev) =>{
-			prev -= Number(item.value);
-			return prev;
-		});
 		setRecord(prev => {
-			prev[day] = newRecord;
-			return prev;
+			prev[dateString] = newRecord;
+			return {...prev};
 		});
-	};
-
-	const getMarkedDate = () => {
-		let marked = {
-			[dateString]: {selected: true, disableTouchEvent: true},
-		};
-		Object.keys(record ?? {}).forEach(key => {
-			const dayRecord = record[key];
-			if (dayRecord.length > 0) {
-				const _key = `${year}-${month}-${key}`;
-				marked[_key] = {...marked[_key], marked: true, dotColor: 'red'};
-			}
+		setSum((prev) =>{
+			prev[dateString] -= Number(item.value);
+			return {...prev};
 		});
-		return marked;
 	};
 
 	return (<SafeAreaView >
@@ -137,7 +120,29 @@ function App() {
 				/>
 				<Calendar
 					onDayPress={setDate}
-					markedDates={getMarkedDate()}
+					onMonthChange={item => {
+						setRecord([]);
+						setSum({});
+						const _year = String(item.year);
+						const _month = String(item.month).padStart(2, '0');
+						const _day = String(item.day).padStart(2, '0');
+						initRecordList(_year, _month, _day);
+					}}
+					// eslint-disable-next-line react/no-unstable-nested-components
+					dayComponent={props => {
+						const { date: _date, onPress, children } = props;
+						const _key = `${_date.year}-${String(_date.month).padStart(2, '0')}-${String(_date.day).padStart(2, '0')}`;
+						return (
+							<TouchableOpacity
+								style={[styles.dayContainer, dateString === _date.dateString ? styles.selectedDay : '']}
+								onPress={() => { requestAnimationFrame(() => onPress(_date)); }}
+							>
+								<Text style={styles.day}>{children}</Text>
+								<Text style={styles.redText}>{formatNumber(sum[_key])}</Text>
+							</TouchableOpacity>
+						);
+					}}
+					hideExtraDays
 				/>
 				<TextInput
 					onChangeText={e => setInputData(prev => ({...prev, name: e}))}
@@ -150,7 +155,7 @@ function App() {
 					inputMode="decimal"
 					placeholder={language.get('price')}
 				/>
-				<Dropdown
+				<Dropdown style={styles.dropdown}
 					value={group}
 					items={dropdownItem}
 					onChange={e => setGroup(e.value)}
@@ -171,10 +176,13 @@ function App() {
 					onPress={onConfirm}
 					disabled={value.length === 0 || !date}
 				/>
-				<Text style={styles.header}>{language.get('date')}: {dateString.toString()} {language.get('sum')}: {formatNumber(sum)}</Text>
+				<Text style={styles.header}>
+					{language.get('date')}: {dateString.toString()}&nbsp;
+					{language.get('sum')}: {formatNumber(sum[key])}
+				</Text>
 				<ScrollView style={styles.record} nestedScrollEnabled={true}>
 				{
-					record?.[day]?.map(item => {
+					record?.[key]?.map(item => {
 						return <View style={styles.listItem} key={item.id}>
 							<Text>
 								{language.get('shop.name')}: {item.name}{'\n'}
@@ -186,7 +194,7 @@ function App() {
 							<Pressable style={styles.clearButton}
 								onPress={() => onClear(item)}
 							>
-								<Text  style={styles.clearText}>{language.get('clear')}</Text>
+								<Text style={styles.clearYext}>{language.get('clear')}</Text>
 							</Pressable>
 						</View>;
 					})
@@ -198,12 +206,17 @@ function App() {
 }
 
 const styles = StyleSheet.create({
+	dayContainer: { width: '100%' },
+	day: {textAlign: 'center'},
+	selectedDay: {color: 'purple', backgroundColor: 'lightblue'},
+	dropdown: {paddingLeft: '4'},
 	header: {textAlign: 'center'},
 	message: {color:'red'},
 	record: {height: '300', overflow: 'hidden'},
 	listItem: {position: 'relative'},
 	clearButton: {position: 'absolute', top: '0', right: '0'},
-	clearText: {color: 'red'},
+	redText: {color: 'red', textAlign: 'center'},
+	clearYext: {color: 'red'},
 });
 
 export default App;
